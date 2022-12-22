4a20e723732b216ef5cf1475d00edfbbada5a370
fulfil_python <f.turdiyevbek94@gmail.com>
2022-12-22 16:53:42 +0500

    arithmetic_op01

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -3,5 +3,6 @@ x = 10
 #Create a variable 'y' and assign it the value of 5.
 y = 5
 #Create a variable 'result' and assign it the value of x + y.
-print(x + y)
+result = x + y
+print(result)
 #Print the value of the 'result'.
