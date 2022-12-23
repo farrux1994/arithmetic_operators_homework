5c268b098a44d8fcf2223f644aecc41c59ed1e55
fulfil_python <f.turdiyevbek94@gmail.com>
2022-12-23 09:25:59 +0500

    arithmetic_op03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 70
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = - number
 #Print the value of the 'answer'
+print(answer)
